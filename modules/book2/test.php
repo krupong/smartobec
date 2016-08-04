c8751ad5c89d2efbc2c593b5508a794d93ac6ad8
krupong <krupong@gmail.com>
2016-08-04 14:16:33 +0700

    ทดสอบการเก็บ log

--- modules/book2/test.php
+++ modules/book2/test.php
@@ -2,15 +2,15 @@
 include '../../include/logUtil.php';
 
 // เพิ่ม log ใหม่
-saveBookLog('1113', "ssssss",'ssssssssss','พพพพพพพพพพพพพพ','ดดดดดดดดดดดดดด','allllllllllllllllสสสสสสสสสสสสสส');
+//saveBookLog('1113', "ssssss",'ssssssssss','พพพพพพพพพพพพพพ','ดดดดดดดดดดดดดด','allllllllllllllllสสสสสสสสสสสสสส');
 
 // เพิ่ม log ใน ref_id เดิม
-//addBookLog('1113', "ssssss",'ssssssssss','พพพพพพพพพพพพพพ','ดดดดดดดดดดดดดด','dddddddd');
+addBookLog('1113', "ssssss",'ssssssssss','พพพพพพพพพพพพพพ','ดดดดดดดดดดดดดด','dddddddd','หหหหหหหหหหหหหหหห');
 
 // แสดง log
 $jsons = json_decode(getBooklog('1111'));
 foreach ($jsons as $json) {
-	echo "ค่า book_table = ".$json->book_table. " ค่า book_idsystem = ".$json->book_idsystem." ค่า book_num = ".$json->book_num
-	." ค่า book_processdate = ".$json->book_processdate." ค่า book_refid = ".$json->book_refid."<br />";
+	echo "ค่า book_table = ".$json->book_table. "<br />ค่า book_idsystem = ".$json->book_idsystem."<br /> ค่า book_num = ".$json->book_num
+	."<br />ค่า book_processdate = ".$json->book_processdate."<br />ค่า book_refid = ".$json->book_refid."<br />==========<br />";
 }
 ?>
