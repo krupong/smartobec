fc09c0720e1b831ed729de2e5518c69c055e1d2e
krupong <krupong@gmail.com>
2016-08-04 12:51:57 +0700

    แก้ไขระบบ log

--- include/logUtil.php
+++ include/logUtil.php
@@ -2,23 +2,23 @@
 // Class เก็บล็อกของ book
 include ("class.smartLog.php");
 
-function saveBookLog($id,$data1,$data2,$data3) {
+function saveBookLog($id,$data1,$data2,$data3,$data4,$data5) {
 	$gen =new smartLog();
-	
+
 	// key ที่ต้องการ ในที่นี้คือ user,level,date
-	$jsonString = '{"user":"'.$data1.'","level":"'.$data2.'","date":"'.$data2.'"}';
+	$jsonString = '{"book_table":"'.$data1.'","book_idsystem":"'.$data2.'","book_num":"'.$data3.'","book_processdate":"'.$data4.'","book_refid":"'.$data5.'"}';
 	$gen = new smartLog();
 	$gen->setRef_id($id);
 	$gen->setLogdata($jsonString);
 	$gen->insert();
 }
 
-function addBookLog($id,$data1,$data2,$data3) {
+function addBookLog($id,$data1,$data2,$data3,$data4,$data5) {
 	$gen = new smartLog();
 	$gen->selectWhere('ref_id',$id);
-	
+
 	// key ที่ต้องการ ในที่นี้คือ user,level,date
-	$jsonString = $gen->getLogdata().',{"user":"'.$data1.'","level":"'.$data2.'","date":"'.$data2.'"}';
+	$jsonString = $gen->getLogdata().',{"book_table":"'.$data1.'","book_idsystem":"'.$data2.'","book_num":"'.$data3.'","book_processdate":"'.$data4.'","book_refid":"'.$data5.'"}';
 	$gen->setRef_id($id);
 	$gen->setLogdata($jsonString);
 	$gen->update($gen->getId());
